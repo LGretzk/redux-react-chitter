500a50029bed7c130ec0eab54c13ea66c41c95e8
LGretzk <LGretzk@protonmail.com>
2022-06-24 18:07:08 +0100

    Add components directory and Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,11 @@
+import {Link} from 'react-router-dom';
+
 function Header() {
   return (
     <div>
       <h1>This the header</h1>
+      <Link to="">Home</Link><br></br>
+      <Link to="peeps">Peeps</Link>
     </div>
   );
 }
