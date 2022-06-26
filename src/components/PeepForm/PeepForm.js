626be305e88aea59e0f2718820413f8fbe4bf40e
LGretzk <LGretzk@protonmail.com>
2022-06-26 12:37:01 +0100

    Add handleSubmitPeep to the PeepForm

--- src/components/PeepForm/PeepForm.js
+++ src/components/PeepForm/PeepForm.js
@@ -1,9 +1,21 @@
 function PeepForm() {
+
+  let sessionKey = sessionStorage.getItem('sessionKey');
+  let userId = sessionStorage.getItem('userId');
+
+  const handleSubmitPeep = async (event) => {
+    event.preventDefault();
+    console.log(event.target);
+    console.log('sess key is ' + sessionKey);
+    console.log('userId is ' + userId);
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmitPeep}>
         <input type="text" name="message" placeholder="What's on your mind?"></input>
-        <input type="hidden"></input>
+        <input type="hidden" name="sessionKey" value={sessionKey}></input>
+        <input type="hidden" name="userId" value={userId}></input>
         <button type="submit" value="submitPeep">Peep</button>
       </form>
     </div>
