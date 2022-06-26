58bf3a7447b83e4dd6813afd9f408dba3e50eafa
LGretzk <LGretzk@protonmail.com>
2022-06-26 16:26:04 +0100

    Add button for deleting peeps

--- src/components/Peep/PeepCard.js
+++ src/components/Peep/PeepCard.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Button from '../Button/Button';
 
 import './PeepCard.css';
 
 function PeepCard(props) {
   const {data} = props;
 
+  const handleClick = () => {
+    console.log('button clicked')
+  }
+
   return (
+    <div>
     <Link to={`/peeps/${data.id}`}>
       <div className="peep-container">
         <p>{data.body}</p>
@@ -14,6 +20,8 @@ function PeepCard(props) {
         <p>{data.created_at}</p>
       </div>
     </Link>
+    <Button onClick={handleClick} />
+    </div>
   );
 }
 
