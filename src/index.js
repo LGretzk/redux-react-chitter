5846bb5e9a3d8ee03293a7540ef646f091d9daad
LGretzk <LGretzk@protonmail.com>
2022-06-27 15:43:02 +0100

    Add provider, configureStore(), rootReducer

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import { rootReducer } from './store/rootReducer';
+
+// Intitializes redux store
+const store = configureStore({
+  reducer: rootReducer
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
 
