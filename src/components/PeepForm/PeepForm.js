588744dcbc59d437dd94a1768106125ce64a04dc
LGretzk <LGretzk@protonmail.com>
2022-06-28 08:24:58 +0100

    Fix PeepForm

--- src/components/PeepForm/PeepForm.js
+++ src/components/PeepForm/PeepForm.js
@@ -24,9 +24,9 @@ function PeepForm() {
     <div>
       <form onSubmit={handleSubmitPeep}>
         <input type="text" name="message" placeholder="What's on your mind?" onFocus={handleFocus}></input>
-        <input type="hidden" name="sessionKey" value={sessionKey}></input>
-        <input type="hidden" name="userId" value={userId}></input>
-        <button type="submit" value="submitPeep">Peep</button>
+        <input type="hidden" name="sessionKey" value={sessionKey || ''}></input>
+        <input type="hidden" name="userId" value={userId || ''}></input>
+        <button type="submit">Peep</button>
       </form>
     </div>
   );
